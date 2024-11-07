4e1fdf7cd61a37d5db99bf5ae21a934b712c5057
sadiaism <ismailmoiz80@gmail.com>
2024-11-07 22:39:29 +0500

    5th assignment

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header=()=>{
+    return(
+        <header>
+        <div className="w-[1920px] h-[134px] bg-[#A29875]">
+            <h1 className=" w-[439px] h-[94] text-[75px] font-[Rye] mt-[20px] ml-[36px] text-[#FFFFFF] ">MANZARRI</h1>
+             
+        </div>
+
+        </header>
+
+        
+        
+    )
+};
+export default Header;
